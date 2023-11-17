64d3a97cac9572763ce3f471c2eeed61f03bead1
Sebastian Ballen <sebastian.ballenc@gmail.com>
2023-11-16 23:02:45 -0500

    feat: 01 Class: Second playground & learning about constants & variables

---
+++ 02_var_let.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// Use let for constants
+let maximumNumberOfLoginAttempts = 3
+
+// Use var for variables
+var currentLoginAttempt = 0
+
+// You can define multiple variables just in one line
+var x = 0.0, y = 0.0, z = 0.0
