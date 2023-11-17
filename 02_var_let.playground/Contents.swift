1dd3c0c5950f1b1155fe095c1f11b448fd1b15b1
Sebastian Ballen <sebastian.ballenc@gmail.com>
2023-11-16 23:49:16 -0500

    feat: Class 03: learning about type annotation & how types works on swift

--- 02_var_let.playground/Contents.swift
+++ 02_var_let.playground/Contents.swift
@@ -8,3 +8,11 @@ var currentLoginAttempt = 0
 
 // You can define multiple variables just in one line
 var x = 0.0, y = 0.0, z = 0.0
+
+// You can define the type of a variable if the value isnt defined
+var welcomeMessage: String
+
+welcomeMessage = "Hola, ¿que tal?"
+
+//This works for multiples variables too
+var red, green, blue: Double
