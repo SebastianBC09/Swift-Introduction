58cc73205630ab6602d375626404918ac45de2a9
Sebastian Ballen <sebastian.ballenc@gmail.com>
2023-11-20 22:35:04 -0500

    feat: Class 04 - Learning about nomenclature for variables & the particular ways you can use other elements like symbols, emojis in swift for this

--- 02_var_let.playground/Contents.swift
+++ 02_var_let.playground/Contents.swift
@@ -9,6 +9,8 @@ var currentLoginAttempt = 0
 // You can define multiple variables just in one line
 var x = 0.0, y = 0.0, z = 0.0
 
+/*                                                                */ 
+
 // You can define the type of a variable if the value isnt defined
 var welcomeMessage: String
 
@@ -16,3 +18,22 @@ welcomeMessage = "Hola, ¿que tal?"
 
 //This works for multiples variables too
 var red, green, blue: Double
+
+/*                                                                */ 
+
+// Camel Case nomenclature for swift variables
+let maximunNumberOfLoginAttempts = 5
+
+// Snake Case nomenclature for variables, not swift standar
+let maximun_number_of_login_attemps = 3
+
+// Swift have some symbols that can be used for variables
+let π = 3.141592
+
+// You can even use emojis as names for variables
+let 👽 = "Alien"
+
+print(👽)
+👽 = "Alien 2"
+
+/*                                                                */ 
